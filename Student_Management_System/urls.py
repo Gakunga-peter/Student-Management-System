16874c30d30a5fb2a2cf2a86148cbef22144f36a
Gakunga Peter <gakungakyle@gmail.com>
2023-07-02 21:13:39 +0300

    intial commit

---
+++ Student_Management_System/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .import views ,Hod_views, Student_views, Staff_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('base/', views.BASE, name='base'),
+    # LOGIN PATH
+    path('', views.LOGIN, name='login'),
+    path('doLogin/', views.doLogin, name='doLogin'),
+
+    # This is HOD panel url
+    path('Hod/Home', Hod_views.HOME, name='home'),
+
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
